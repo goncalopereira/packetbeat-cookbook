5e28c324e0ac70c087472e507f00a693b9becafd
goncalo <goncalo@goncalopereira.com>
2014-05-20 17:31:04 +0100

    client recipe

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,2 +1,4 @@
 node.set['java']['jdk_version'] = "7"
 node.set['java']['oracle']['accept_oracle_download_terms']=true
+
+node.set['packetbeat']['server'] = "192.168.99.13" 
--- recipes/default.rb
+++ recipes/default.rb
@@ -1,3 +1,34 @@
 #
 # Cookbook Name:: packetbeat
-# Recipe:: default
+# Recipe:: client
+
+yum_package "libpcap"
+
+remote_file "/tmp/packetbeat-0.1.1-1.el6.x86_64.rpm" do
+  source "https://github.com/packetbeat/packetbeat/releases/download/v0.1.1/packetbeat-0.1.1-1.el6.x86_64.rpm"
+end
+
+remote_file "/tmp/daemonize-1.7.3-1.el6.x86_64.rpm" do
+  source "ftp://ftp.hosteurope.de/mirror/fedora-epel/6/x86_64/daemonize-1.7.3-1.el6.x86_64.rpm"
+end
+
+rpm_package "daemonize" do
+	source "/tmp/daemonize-1.7.3-1.el6.x86_64.rpm"
+	action :install
+end
+
+rpm_package "packetbeat" do
+	source "/tmp/packetbeat-0.1.1-1.el6.x86_64.rpm"
+	action :install
+end
+
+template "/etc/packetbeat/packetbeat.conf" do
+  source "packetbeat.conf.erb"
+  mode 00644
+  notifies :restart, "service[packetbeat]", :delayed
+end
+
+service "packetbeat" do
+  supports :restart => true, :reload => true
+  action [:enable, :start]
+end
